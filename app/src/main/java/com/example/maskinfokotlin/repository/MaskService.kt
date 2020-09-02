eba210254a6ca924b03a6781f48e3cb41ba85fae
Junsuk Oh <a811219@gmail.com>
2020-09-02 10:09:55 +0900

    공적마스크 API 중단으로 백업 API로 대체

--- app/src/main/java/com/example/maskinfokotlin/repository/MaskService.kt
+++ app/src/main/java/com/example/maskinfokotlin/repository/MaskService.kt
@@ -12,6 +12,8 @@ interface MaskService {
     ): StoreInfo
 
     companion object {
-        const val BASE_URL = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/"
+        // const val BASE_URL = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/"
+        // 공적마스크 API 중단으로 백업 API로 대체하였음
+        const val BASE_URL = "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/"
     }
 }
