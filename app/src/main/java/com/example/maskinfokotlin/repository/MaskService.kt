5b1a7336ba64ab02b9a8aef760df17e051e12aa1
Junsuk Oh <a811219@gmail.com>
2020-09-07 20:33:23 +0900

    백업용 base_url 적용

--- app/src/main/java/com/example/maskinfokotlin/repository/MaskService.kt
+++ app/src/main/java/com/example/maskinfokotlin/repository/MaskService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MaskService {
-    @GET("storesByGeo/json/?m=5000")
+    @GET("bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/")
     suspend fun fetchStoreInfo(
         @Query("lat") lat: Double,
         @Query("lng") lng: Double
@@ -14,6 +14,6 @@ interface MaskService {
     companion object {
         // const val BASE_URL = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/"
         // 공적마스크 API 중단으로 백업 API로 대체하였음
-        const val BASE_URL = "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/"
+        const val BASE_URL = "https://gist.githubusercontent.com/junsuk5/"
     }
 }
